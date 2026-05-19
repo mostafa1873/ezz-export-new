79260db17cbb7e7f2b362aa5b9c69d6c4bc831e9
mostafa1873 <os8722398@gmail.com>
2026-05-20 00:33:44 +0300

    u-0

--- src/components/home/whyus.tsx
+++ src/components/home/whyus.tsx
@@ -26,7 +26,7 @@ const AboutFluid = () => {
     visible: { 
       opacity: 1, 
       y: 0, 
-      transition: { duration: 0.5, ease: [0.215, 0.610, 0.355, 1.000] } 
+      transition: { duration: 0.5, ease: [0.215, 0.610, 0.355, 1.000] as const } 
     }
   };
 
